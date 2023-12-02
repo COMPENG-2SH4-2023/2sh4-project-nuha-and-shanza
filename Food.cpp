1dd2f9523030e591b50f189ef44536747268ca9d
riazs9 <riazs9@mcmaster.ca>
2023-12-02 15:03:53 -0500

    final sumbission 2

--- Food.cpp
+++ Food.cpp
@@ -21,39 +21,37 @@ Food::~Food()
 
 void Food::generateFood(objPosArrayList* blockOffList)
 {
-    // Seed the random integer generation function with current time.
+    // Seed the random integer generation function with current time
     srand(time(NULL)); 
+
     int foodX, foodY;
     objPos tempPlayer;
-    bool drawn;
+    bool setFood = false;
     
-    while(true)
+    while(setFood == false) //keep generating random coordinates
     {
-        drawn = false;
-        //random coordinate generation
+        setFood = true;
+        
+        //random item generation
         foodX = (rand() % (mainGameMechsRef->getBoardSizeX() - 2) + 1);
         foodY = (rand() % (mainGameMechsRef->getBoardSizeY() - 2) + 1); 
-    
-        //if food is NOT on current player position, draw food on board
+
         for (int i = 0; i<blockOffList->getSize(); i++)
         {
-            blockOffList->getElement(tempPlayer,i);
-            if(foodX != tempPlayer.x && foodY != tempPlayer.y)
-                {
-                    foodPos.x = foodX;
-                    foodPos.y = foodY;
-                    drawn = true;
-                    break;
-                }
+            blockOffList->getElement(tempPlayer,i); //get each element of player
 
+            if(foodX == tempPlayer.x && foodY == tempPlayer.y)
+            {
+                //if food is generated anywhere on snake body, do not set it as food position
+                setFood = false;
+                break; //generate new coordinate for food
+            }
+         
         }
-
-       if (drawn)
-        {
-            break;  // Break out of the while loop when a suitable food position is found
-        } 
-
     }
+    //food position set in randomly generated coordinate
+    foodPos.x = foodX;
+    foodPos.y = foodY;
 
 }
     
